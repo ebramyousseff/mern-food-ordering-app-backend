76c7f13fdc85709eab8eb0e74a0eb430f2e765ad
Ebram-yousseff <youssefsaaad63@gmail.com>
2025-12-21 23:50:23 +0200

    first commit

---
+++ src/routes/MyUserRoute.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import MyUserControllers from '../controllers/MyUserControllers';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyUserRequest } from '../middleware/validation';
+
+const router = express.Router();
+
+
+router.get("/",jwtCheck, jwtParse, MyUserControllers.getCurrentUser);
+router.post('/', jwtCheck, MyUserControllers.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserControllers.updateCurrentUser);
+export default router;
+
