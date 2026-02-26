00f4390a73aaff490cec8824ded9d12a9750ef26
Ebram-yousseff <youssefsaaad63@gmail.com>
2026-02-26 07:49:53 +0200

    add user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -6,12 +6,12 @@ const handleValidationErrors = async (
   res: Response,
   next: NextFunction
 ) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()})
-    } 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    next();
+  next();
 };
 
 export const validateMyUserRequest = [
@@ -23,4 +23,28 @@ export const validateMyUserRequest = [
   body("city").isString().notEmpty().withMessage("City must be a String"),
   body("country").isString().notEmpty().withMessage("Country must be a String"),
   handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurent name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a positive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a postive number"),
+    handleValidationErrors,
 ];
